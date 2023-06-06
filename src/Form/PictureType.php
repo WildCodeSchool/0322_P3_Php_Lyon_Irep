a9f2b479c827edaafbaac65e28d45beee02bc358
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-06-06 12:04:12 +0200

    add entity Picture, Crud Picture, Fixtures Picture, First test galery

---
+++ src/Form/PictureType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('reference')
+            ->add('title')
+            ->add('subtitle')
+            ->add('date')
+            ->add('technic')
+            ->add('size')
+            ->add('category')
+            ->add('number')
+            ->add('comment')
+            ->add('link')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
