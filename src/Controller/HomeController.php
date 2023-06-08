1b295860a1c6e6e2f0ef9c1d4c5b6e9159d7214c
lbf-2974 <lb_perso@hotmail.com>
2023-06-09 00:02:33 +0200

    add exhibition fixtures / controller & twig for carousel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ExhibitionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(ExhibitionRepository $exhibitionRepository): Response
+    {   
+        $exhibitions = $exhibitionRepository->findAll();
+
+        return $this->render('home/index.html.twig', 
+        ['exhibitions' => $exhibitions]
+        );
     }
 }
