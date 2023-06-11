901bc52ee868caa55e707c37bf982104002bcd6f
Mouhamed <mouhamediop114@gmail.com>
2023-06-11 21:55:55 +0200

    review my PR

--- assets/app.js
+++ assets/app.js
@@ -3,17 +3,12 @@ import "./styles/app.scss";
 import "./bootstrap";
 import "flowbite";
 
-
 // Activate the dropdown menu language only on click
 document.getElementById('dropdownHoverButton').addEventListener('click', function() {
     document.getElementById('dropdownHover').classList.toggle('hidden');
 });
 
-
-
-
 // Animation to hide the navbar when scrolldown
-
 let lastScrollTop = 0;
 const navbar = document.querySelector('.custom-navbar');
 
@@ -35,8 +30,8 @@ window.addEventListener("scroll", () => {
 let modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "title" text as a caption
-let img = document.getElementById("myImg");
-let modalImg = document.getElementById("img01");
+let img = document.getElementById("my-picture");
+let modalImg = document.getElementById("img-modal");
 let captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
@@ -51,3 +46,5 @@ let span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
+
+
