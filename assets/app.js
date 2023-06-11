84ab6651b5d9ba7a4fd654c85c7182d35dc199c9
Mouhamed <mouhamediop114@gmail.com>
2023-06-11 02:21:53 +0200

    add modal when click on image

--- assets/app.js
+++ assets/app.js
@@ -8,28 +8,8 @@ import "flowbite";
 document.getElementById('dropdownHoverButton').addEventListener('click', function() {
     document.getElementById('dropdownHover').classList.toggle('hidden');
 });
-//------------------------show image -----------------//
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-}
 
 
 // Animation to hide the navbar when scrolldown
@@ -49,3 +29,25 @@ window.addEventListener("scroll", () => {
     }
     lastScrollTop = scrollTop;
 });
+
+//------------------------display the image in full screen -----------------//
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "title" text as a caption
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
