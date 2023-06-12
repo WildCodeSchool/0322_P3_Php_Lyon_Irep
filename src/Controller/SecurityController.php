e4c51e6c4c9bfa96031cbf48dc3814c973e7eb89
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-06-12 11:39:24 +0200

    Delete some dead code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,9 +12,6 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('app_home');
-        // }
 
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
