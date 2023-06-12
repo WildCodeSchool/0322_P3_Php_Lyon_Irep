11568b47ea8ed09c92f13ff3c5df942ca004ed1b
lbf-2974 <lb_perso@hotmail.com>
2023-06-12 12:59:17 +0200

    exhibitions carousel finished

--- src/DataFixtures/ExhibitionFixtures.php
+++ src/DataFixtures/ExhibitionFixtures.php
@@ -10,19 +10,19 @@ class ExhibitionFixtures extends Fixture
 {
     public const EXHIBITIONS = [
         ['title' => "LE TAMPON",
-         'subtitle' =>  "Journées Européennes du Patrimoine 2023.",
+         'subtitle' =>  "Journées Européennes du Patrimoine 2023",
          'image' =>  "build/images/carousel1.png"],
 
-         ['title' => "BEL AIR, D'UNE SUCRERIE A LA NAISSANCE D'UN VILLAGE.",
-         'subtitle' =>  "Visitez la galerie et parcourez les oeuvres.",
+         ['title' => "BEL AIR, D'UNE SUCRERIE A LA NAISSANCE D'UN VILLAGE",
+         'subtitle' =>  "Visitez la galerie et parcourez les oeuvres",
          'image' =>  "build/images/carousel2.png"],
 
-         ['title' => "HIPPOLYTE CHARLES NAPOLEON MORTIER.",
-         'subtitle' =>  "Duc de Trévise.",
+         ['title' => "HIPPOLYTE CHARLES NAPOLEON MORTIER",
+         'subtitle' =>  "Duc de Trévise",
          'image' =>  "build/images/carousel3.png"],
 
          ['title' => "ILE DE LA REUNION EXPOSITIONS PATRIMOINE",
-         'subtitle' => "Une association au service de l'histoire de l'Ile.",
+         'subtitle' => "Une association au service de l'histoire de l'Ile",
          'image' =>  "build/images/carousel3.png"],
         ];
 
