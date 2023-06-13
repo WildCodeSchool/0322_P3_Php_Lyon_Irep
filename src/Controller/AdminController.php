7bbc7d445e4ba7048518dc7235de9ef9e3c193d6
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-06-13 11:11:02 +0200

    csrf add on security.yaml / change authorisation in controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
