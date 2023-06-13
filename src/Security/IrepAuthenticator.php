cdc5bcf775a797831e1ae20aa852f494a7babf7e
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-06-13 15:34:14 +0200

    Change name of authenticator

--- src/Security/IrepAuthenticator.php
+++ src/Security/IrepAuthenticator.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class IrepAuthtenticatorAuthenticator extends AbstractLoginFormAuthenticator
+class IrepAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
