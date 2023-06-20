c228f1a29e88ebf47f651a4fc781decaaeb82452
Mouhamed <mouhamediop114@gmail.com>
2023-06-20 15:27:39 +0200

    image upload form

--- migrations/Version20230620122959.php
+++ migrations/Version20230620122959.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230615185451 extends AbstractMigration
+final class Version20230620122959 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -6,22 +6,48 @@ use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class PictureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('reference')
-            ->add('title')
-            ->add('subtitle')
-            ->add('date')
-            ->add('technic')
-            ->add('size')
-            ->add('category')
-            ->add('number')
-            ->add('comment')
-            ->add('link')
+        ->add('reference', TextType::class, [
+            'label' => 'reference'
+        ])
+        ->add('title', TextType::class, [
+            'label' => 'titre'
+        ])
+        ->add('subtitle', TextType::class, [
+            'label' => 'sous titre'
+        ])
+        ->add('date', TextType::class, [
+            'label' => 'date'
+        ])
+        ->add('technic', TextType::class, [
+            'label' => 'technique'
+        ])
+        ->add('size', TextType::class, [
+            'label' => 'Dimension'
+        ])
+        ->add('category', TextType::class, [
+            'label' => 'catégorie'
+        ])
+        ->add('number', TextType::class, [
+            'label' => 'numéro'
+        ])
+        ->add('comment', TextType::class, [
+            'label' => 'commentaire'
+        ])
+        ->add('photoFile', FileType::class, [
+            'label' => 'Image',
+            'mapped' => false,
+            'required' => false,
+        ])
         ;
     }
 
