03756fc6874f85d32b888378096bff23ec012a96
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-07-11 18:37:06 +0200

    Add burger menu

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,21 @@
 import "./styles/app.scss";
 import "./bootstrap";
 import "flowbite";
+
+
+
+document.addEventListener('click', function(event) {
+    var burgerMenu = document.getElementById('navbar-hamburger');
+    var isClickInsideMenu = burgerMenu.contains(event.target);
+    var isClickOnBurgerButton = document.querySelector('[data-collapse-toggle="navbar-hamburger"]').contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnBurgerButton) {
+        burgerMenu.classList.add('hidden');
+    }
+});
+
+document.querySelector('[data-collapse-toggle="navbar-hamburger"]').addEventListener('click', function() {
+    var burgerMenu = document.getElementById('navbar-hamburger');
+    burgerMenu.classList.toggle('hidden');
+});
+
