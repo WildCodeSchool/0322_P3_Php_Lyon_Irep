fc05d0d5b4335af4fababe77c0da6974ef73efe1
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-07-12 14:47:46 +0200

    Add session on navbar, and permanent link depending the exhibition

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,4 +1,3 @@
-
 document.querySelector('[data-collapse-toggle="navbar-hamburger"]').addEventListener('click', function() {
     const burgerMenu = document.getElementById('navbar-hamburger');
     burgerMenu.classList.toggle('hidden');
@@ -7,6 +6,16 @@ document.querySelector('[data-collapse-toggle="navbar-hamburger"]').addEventList
 document.querySelectorAll('.group ul a').forEach(function(link) {
     link.addEventListener('click', function(event) {
         event.preventDefault();
+
+        fetch('/save-exhibition-id', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                exhibitionId: event.currentTarget.getAttribute('data-exhibition-id')
+            })
+        });
 
         const existingGalleryLink = document.querySelector('.navbar-galerie');
         if (existingGalleryLink) existingGalleryLink.remove();
@@ -17,5 +26,23 @@ document.querySelectorAll('.group ul a').forEach(function(link) {
 
         const navbarItems = document.querySelector('.md\\:flex');
         navbarItems.insertBefore(galleryLink, navbarItems.childNodes[Math.floor(navbarItems.childNodes.length / 2)]);
+
+        localStorage.setItem('galleryUrl', link.href);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryUrl = localStorage.getItem('galleryUrl');
+    if (galleryUrl) {
+        const existingGalleryLink = document.querySelector('.navbar-galerie');
+        if (existingGalleryLink) existingGalleryLink.remove();
+
+        const galleryLink = document.createElement('li');
+        galleryLink.className = 'navbar-galerie';
+        galleryLink.innerHTML = '<a href="' + galleryUrl + '" class="px-4 py-2 justify-self-start pr-60 mr-60 text-white hover:text-gray-200">Galerie</a>';
+
+        const navbarItems = document.querySelector('.md\\:flex');
+        navbarItems.insertBefore(galleryLink, navbarItems.childNodes[Math.floor(navbarItems.childNodes.length / 2)]);
+    }
+});
+
--- src/Components/ExhibitionComponent.php
+++ src/Components/ExhibitionComponent.php
@@ -3,20 +3,29 @@
 namespace App\Components;
 
 use App\Repository\ExhibitionRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 class ExhibitionComponent
 {
     private ExhibitionRepository $exhibitionRepository;
+    private SessionInterface $session;
 
-    public function __construct(ExhibitionRepository $exhibitionRepository)
+    public function __construct(ExhibitionRepository $exhibitionRepository, RequestStack $requestStack)
     {
         $this->exhibitionRepository = $exhibitionRepository;
+        $this->session = $requestStack->getSession();
     }
 
     public function getExhibitions(): array
     {
         return $this->exhibitionRepository->findCurrentExhibitions();
+    }
+
+    public function getSelectedExhibition(): ?int
+    {
+        return $this->session->get('exhibitionId');
     }
 }
